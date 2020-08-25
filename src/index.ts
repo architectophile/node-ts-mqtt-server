1f98ca6d7af2d983171ca4d872a4cd4a0af5b84e
Byeongcheol Yoo <architectophile@gmail.com>
2020-08-25 15:46:35 +0900

    mosca added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-console.log("Hello World! This is the first TypeScript project!");
+import mosca, { Client, Packet } from "mosca";
 
-for (let i = 0; i < 100; i++) {
-  console.log(i);
-}
+const settings = {
+  port: 1883,
+};
+
+const mqtt_server = new mosca.Server(settings);
+
+mqtt_server.on("ready", function () {
+  console.log("MQTT broker is ready.");
+});
+
+mqtt_server.on("clientConnected", (client: Client) => {
+  console.log("on clientConnected called.");
+  console.log("client: ", client);
+});
+
+mqtt_server.on("published", (packet: Packet, client: Client) => {
+  console.log("on published called.");
+  console.log("packet: ", packet);
+  console.log("client: ", client);
+});
