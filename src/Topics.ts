d1d67be37600324db8672a425068a77ad81abda1
Byeongcheol Yoo <architectophile@gmail.com>
2020-08-30 18:28:38 +0900

    Relay added

---
+++ src/Topics.ts
@@ -0,0 +1,18 @@
+// nCube Subscribe topics
+export const TOPIC_NCUBE_CONTROL_AUTH_START = "/nCube/Control/Auth/start";
+export const TOPIC_NCUBE_CONTROL_AUTH_REQ_AUTH = "/nCube/Control/Auth/req_auth";
+export const TOPIC_NCUBE_CONTROL_ENC_REQ_ENC = "/nCube/Control/Enc/req_enc";
+export const TOPIC_NCUBE_CONTROL_SIG_REQ_SIG = "/nCube/Control/Sig/req_sig";
+
+// nCube Publish topics
+export const TOPIC_NCUBE_DATA_AUTH_RES_AUTH = "/nCube/Data/Auth/res_auth";
+export const TOPIC_NCUBE_DATA_ENC_RES_ENC = "/nCube/Data/Enc/res_enc";
+export const TOPIC_NCUBE_DATA_SIG_RES_SIG = "/nCube/Data/Sig/res_sig";
+
+// Mobius Subscribe topics
+export const TOPIC_MOBIUS_CONTROL_REQ_AUTH = "/mobius/Control/Req_auth";
+
+// Mobius Publish topics
+export const TOPIC_MOBIUS_DATA_RES_AUTH = "/mobius/Data/Res_auth";
+export const TOPIC_MOBIUS_DATA_RESULT_AUTH = "/mobius/Data/Result_auth";
+export const TOPIC_MOBIUS_DATA_CERTIFICATION = "/mobius/Data/Certification";
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import mosca, { Client, Packet } from "mosca";
+import mqtt, { Packet as ClientPacket } from "mqtt";
+
+import {
+  TOPIC_MOBIUS_CONTROL_REQ_AUTH,
+  TOPIC_MOBIUS_DATA_RES_AUTH,
+  TOPIC_NCUBE_CONTROL_AUTH_START,
+  TOPIC_NCUBE_CONTROL_AUTH_REQ_AUTH,
+  TOPIC_NCUBE_DATA_AUTH_RES_AUTH,
+  TOPIC_NCUBE_DATA_ENC_RES_ENC,
+  TOPIC_NCUBE_DATA_SIG_RES_SIG,
+} from "./Topics";
 
 const settings = {
   port: 1883,
@@ -8,6 +19,7 @@ const mqtt_server = new mosca.Server(settings);
 
 mqtt_server.on("ready", function () {
   console.log("MQTT broker is ready.");
+  startRelayClient();
 });
 
 mqtt_server.on("clientConnected", (client: Client) => {
@@ -17,6 +29,99 @@ mqtt_server.on("clientConnected", (client: Client) => {
 
 mqtt_server.on("published", (packet: Packet, client: Client) => {
   console.log("on published called.");
-  console.log("packet: ", packet);
-  console.log("client: ", client);
+  // console.log("packet: ", packet);
+  // console.log("client: ", client);
 });
+
+const startRelayClient = () => {
+  const isSecure = false;
+  const host = "localhost";
+  const port = isSecure ? "8883" : "1883";
+  const protocol = isSecure ? "mqtts" : "mqtt";
+  const protocolId = "MQTT";
+  const protocolVersion = 4;
+
+  const connectOptions = {
+    host,
+    port,
+    protocol,
+    keepalive: 10,
+    protocolId,
+    protocolVersion,
+    clean: true,
+    reconnectPeriod: 2000,
+    connectTimeout: 2000,
+    // key: fs.readFileSync("./server-key.pem"),
+    // cert: fs.readFileSync("./server-crt.pem"),
+    rejectUnauthorized: false,
+  };
+
+  const mqtt_client = mqtt.connect(connectOptions);
+
+  mqtt_client.on("connect", () => {
+    mqtt_client.subscribe(TOPIC_NCUBE_DATA_AUTH_RES_AUTH);
+    console.log(
+      "subscribe nCube auth response topic at " +
+        TOPIC_NCUBE_DATA_AUTH_RES_AUTH,
+    );
+
+    mqtt_client.subscribe(TOPIC_NCUBE_DATA_ENC_RES_ENC);
+    console.log(
+      "subscribe nCube encryption response topic at " +
+        TOPIC_NCUBE_DATA_ENC_RES_ENC,
+    );
+
+    mqtt_client.subscribe(TOPIC_NCUBE_DATA_SIG_RES_SIG);
+    console.log(
+      "subscribe nCube signing response topic at " +
+        TOPIC_NCUBE_DATA_SIG_RES_SIG,
+    );
+
+    mqtt_client.subscribe(TOPIC_MOBIUS_DATA_RES_AUTH);
+    console.log(
+      "subscribe Mobius auth response topic at " + TOPIC_MOBIUS_DATA_RES_AUTH,
+    );
+
+    setInterval(() => {
+      mqtt_client.publish(TOPIC_NCUBE_CONTROL_AUTH_START, "");
+    }, 8000);
+  });
+
+  mqtt_client.on(
+    "message",
+    (topic: string, message: Buffer, packet: ClientPacket) => {
+      switch (topic) {
+        case TOPIC_NCUBE_DATA_AUTH_RES_AUTH: {
+          mqtt_client.publish(
+            TOPIC_MOBIUS_CONTROL_REQ_AUTH,
+            message.toString(),
+          );
+          break;
+        }
+        case TOPIC_NCUBE_DATA_ENC_RES_ENC: {
+          mqtt_client.publish(
+            TOPIC_MOBIUS_CONTROL_REQ_AUTH,
+            message.toString(),
+          );
+          break;
+        }
+        case TOPIC_NCUBE_DATA_SIG_RES_SIG: {
+          mqtt_client.publish(
+            TOPIC_MOBIUS_CONTROL_REQ_AUTH,
+            message.toString(),
+          );
+          break;
+        }
+        case TOPIC_MOBIUS_DATA_RES_AUTH: {
+          mqtt_client.publish(
+            TOPIC_NCUBE_CONTROL_AUTH_REQ_AUTH,
+            message.toString(),
+          );
+          break;
+        }
+        default:
+          return;
+      }
+    },
+  );
+};
